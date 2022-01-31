1d3a6b554f98ecbad9d621dd4d2165ad50aff7a3
Sergey <gitkhartmann.com>
2022-01-31 12:08:32 +0300

    изучил динамические компоненты

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver, ViewChild } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+import { ModalComponent } from './modal/modal.component';
+import { RefDirective } from './ref.directive';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular_dincomponent';
+	@ViewChild(RefDirective) refDir!: RefDirective
+
+	constructor(private resolver: ComponentFactoryResolver, private title: Title, private meta: Meta) {
+		this.meta.addTags([
+			{ name: 'Petya', content: 'amgular, googleeeee' },
+			{ name: 'description', content: 'this is app component' }
+		])
+		title.setTitle('AppComponent Page');
+	}
+	showModal() {
+		const modalFactory = this.resolver.resolveComponentFactory(ModalComponent);
+		const component = this.refDir.containerRef.createComponent(modalFactory);
+		component.instance.title = 'Dynamic title';
+		component.instance.close.subscribe(() => {
+			this.refDir.containerRef.clear();
+		})
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ModalComponent } from './modal/modal.component';
+import { RefDirective } from './ref.directive';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		ModalComponent,
+		RefDirective
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+  ServiceWorkerModule.register('ngsw-worker.js', {
+    enabled: environment.production,
+    // Register the ServiceWorker as soon as the app is stable
+    // or after 30 seconds (whichever comes first).
+    registrationStrategy: 'registerWhenStable:30000'
+  })
+	],
+	providers: [],
+	entryComponents: [ModalComponent],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
